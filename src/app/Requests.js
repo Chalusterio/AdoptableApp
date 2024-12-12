b0d93b32500ed6cc23884f417ee312d615e8309a
Esther Eblacas <eblacas.estherbeuthel@gmail.com>
2024-12-13 03:28:54 +0800

    Feed: used memo; Improved some of the UI

--- src/app/Requests.js
+++ src/app/Requests.js
@@ -144,6 +144,7 @@ const Requests = () => {
       <SafeAreaView style={styles.safeArea}>
         <Surface style={styles.titleContainer} elevation={3}>
           <Text style={styles.title}>Your Pending Requested Pets</Text>
+          <Text style={styles.instruction}>Press and hold the pet card to cancel your request.</Text>
         </Surface>
 
         {isLoading ? ( // Display loading spinner while fetching data
@@ -190,6 +191,11 @@ const styles = StyleSheet.create({
   title: {
     fontFamily: "Lilita",
     fontSize: 24,
+    color: "#68C2FF",
+  },
+  instruction: {
+    fontFamily: "Lato",
+    fontSize: 16,
     color: "#68C2FF",
   },
   container: {
