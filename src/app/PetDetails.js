5d88b1e0d4cf0ee113eeefa783d367a6a17d2e7d
Esther Eblacas <eblacas.estherbeuthel@gmail.com>
2024-12-13 15:21:40 +0800

    Revert "PetDetails, Screening: back to zero"
    This reverts commit da8d8c596b3a75c220273f2a03094f64cffc4939.

--- src/app/PetDetails.js
+++ src/app/PetDetails.js
@@ -18,7 +18,6 @@ import { usePets } from '../context/PetContext'; // Import the context
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { db, auth } from "../../firebase"; // Ensure `auth` is imported from Firebase
-import { useNotifications } from '../context/NotificationContext';  // Import the hook
 
 const screenWidth = Dimensions.get("window").width;
 
